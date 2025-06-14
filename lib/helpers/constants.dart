d14c879d0f8001335cbbbac70c5209e9b6223bfd
Luis Núñez <luisalbertonu@gmail.com>
2025-06-14 10:31:02 -0300

    Fotos en Grilla

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -2,5 +2,5 @@ class Constants {
   static String get apiAppParametros =>
       'http://www.gaos2.keypress.com.ar/KPAppParametros/';
 
-  static String get version => '1.0.1';
+  static String get version => '1.0.2';
 }
--- lib/screens/obras/obra_info_screen.dart
+++ lib/screens/obras/obra_info_screen.dart
@@ -26,10 +26,10 @@ class ObraInfoScreen extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  _ObraInfoScreenState createState() => _ObraInfoScreenState();
+  ObraInfoScreenState createState() => ObraInfoScreenState();
 }
 
-class _ObraInfoScreenState extends State<ObraInfoScreen> {
+class ObraInfoScreenState extends State<ObraInfoScreen> {
 //----------------------- Variables -----------------------------
   bool _photoChanged = false;
   late XFile _image;
@@ -73,6 +73,8 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
   List<ObrasDocumento> _obrasDocumentos = [];
   List<ObrasDocumento> _obrasDocumentosFotos = [];
 
+  final List<bool> _isSelected = [true, false];
+
 //----------------------- initState -----------------------------
   @override
   void initState() {
@@ -84,35 +86,182 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
 //----------------------- Pantalla -----------------------------
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('Obra ${widget.obra.nroObra}'),
         centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Container(
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: ToggleButtons(
+                    fillColor: Colors.lightBlue,
+                    //        splashColor: Colors.purple,
+                    direction:
+                        Axis.horizontal, // Direccion horizontal o vertical
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int i = 0; i < _isSelected.length; i++) {
+                          if (i == index) {
+                            _isSelected[i] = !_isSelected[i];
+                          } else {
+                            _isSelected[i] =
+                                false; // Deseleccionar los otros botones
+                          }
+                        }
+                      });
+                    },
+                    isSelected: _isSelected,
+                    children: const <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: Text(' Carousel ',
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.white)),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(2.0),
+                        child: Text('   Grilla   ',
+                            style:
+                                TextStyle(fontSize: 16, color: Colors.white)),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
       body: Stack(
         children: [
           Column(
             children: [
               _getInfoObra(),
-              Expanded(
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: <Widget>[
-                      _obrasDocumentosFotos.isNotEmpty
-                          // ? Container(
-                          //     width: 300,
-                          //     height: 200,
-                          //     color: Colors.amberAccent,
-                          //     child:
-                          //         Text(_obrasDocumentosFotos.length.toString()),
-                          //   )
-                          ? _showPhotosCarousel()
-                          : Container(),
-                    ],
+              if (_isSelected[0])
+                Expanded(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        _obrasDocumentosFotos.isNotEmpty
+                            // ? Container(
+                            //     width: 300,
+                            //     height: 200,
+                            //     color: Colors.amberAccent,
+                            //     child:
+                            //         Text(_obrasDocumentosFotos.length.toString()),
+                            //   )
+                            ? _showPhotosCarousel()
+                            : Container(),
+                      ],
+                    ),
                   ),
                 ),
-              ),
+              if (!_isSelected[0])
+                Expanded(
+                  child: GridView.builder(
+                    itemCount: _obrasDocumentos.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // 3 columnas
+                      crossAxisSpacing: 8.0,
+                      mainAxisSpacing: 8.0,
+                    ),
+                    itemBuilder: (context, index) {
+                      final obra = _obrasDocumentos[index];
+                      return GestureDetector(
+                        onTap: () {
+                          showDialog(
+                              context: context,
+                              builder: (_) {
+                                return Stack(
+                                  children: [
+                                    InteractiveViewer(
+                                      boundaryMargin:
+                                          const EdgeInsets.all(double.infinity),
+                                      child: SizedBox(
+                                        width: size.width,
+                                        height: size.height,
+                                        child: FadeInImage.assetNetwork(
+                                          placeholder: 'assets/loading.gif',
+                                          image: obra.imageFullPath!,
+                                          fit: BoxFit.contain,
+                                          width: size.width,
+                                          height: size.height,
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                        top: size.height * 0.08,
+                                        right: 5,
+                                        child: IconButton(
+                                          icon: const Icon(
+                                            FontAwesomeIcons.rectangleXmark,
+                                            color: Colors.red,
+                                            size: 36,
+                                          ),
+                                          onPressed: () {
+                                            Navigator.of(context).pop();
+                                          },
+                                        ))
+                                  ],
+                                );
+                              });
+                        },
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: CachedNetworkImage(
+                                imageUrl: obra.imageFullPath!,
+                                placeholder: (context, url) =>
+                                    Image.asset('assets/loading.gif'),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                                fit: BoxFit.cover,
+                                memCacheHeight: 300, // ajusta según tu diseño
+                                memCacheWidth: 300,
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 5,
+                              left: 5,
+                              child: Text(
+                                obra.tipoDeFoto == 0
+                                    ? 'Relevamiento(Vereda/Calzada/Traza)'
+                                    : obra.tipoDeFoto == 1
+                                        ? 'Previa al trabajo'
+                                        : obra.tipoDeFoto == 2
+                                            ? 'Durante el trabajo'
+                                            : obra.tipoDeFoto == 3
+                                                ? 'Vereda conforme'
+                                                : obra.tipoDeFoto == 4
+                                                    ? 'Finalización del Trabajo'
+                                                    : obra.tipoDeFoto == 5
+                                                        ? 'Proceso de geofonía'
+                                                        : obra.tipoDeFoto == 6
+                                                            ? 'Proceso de reparación'
+                                                            : '',
+                                style: const TextStyle(
+                                    color: Colors.white, fontSize: 12),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
               _showImageButtons(),
               const SizedBox(
                 height: 5,
@@ -171,19 +320,6 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
                           ))
                       : Container(),
                 ),
-                // const Text('Módulo: ',
-                //     style: TextStyle(
-                //       fontSize: 12,
-                //       color: AppTheme.primary,
-                //       fontWeight: FontWeight.bold,
-                //     )),
-                // Expanded(
-                //   flex: 4,
-                //   child: Text(_obra.modulo.toString(),
-                //       style: const TextStyle(
-                //         fontSize: 12,
-                //       )),
-                // ),
               ],
             ),
             const SizedBox(
@@ -242,7 +378,7 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
         children: [
           CarouselSlider(
             options: CarouselOptions(
-                height: size.height * 0.65,
+                height: size.height * 0.60,
                 autoPlay: false,
                 initialPage: 0,
                 autoPlayInterval: const Duration(seconds: 0),
@@ -361,26 +497,59 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
               );
             }).toList(),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: _obrasDocumentosFotos.asMap().entries.map((entry) {
-              return GestureDetector(
-                onTap: () => _carouselController.animateToPage(entry.key),
-                child: Container(
-                  width: 12.0,
-                  height: 12.0,
-                  margin: const EdgeInsets.symmetric(
-                      vertical: 8.0, horizontal: 4.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: (Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black)
-                          .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+          if (_obrasDocumentosFotos.length > 12)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.arrow_back_ios, size: 26),
+                  onPressed: () {
+                    _carouselController.previousPage();
+                  },
                 ),
-              );
-            }).toList(),
+                const SizedBox(
+                  width: 50,
+                ),
+                Text('${_current + 1} / ${_obrasDocumentosFotos.length}',
+                    style: const TextStyle(fontSize: 26)),
+                const SizedBox(
+                  width: 50,
+                ),
+                IconButton(
+                  icon: const Icon(
+                    Icons.arrow_forward_ios,
+                    size: 26,
+                  ),
+                  onPressed: () {
+                    _carouselController.nextPage();
+                  },
+                ),
+              ],
+            ),
+          const SizedBox(
+            height: 20,
           ),
+          if (_obrasDocumentosFotos.length <= 12)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _obrasDocumentosFotos.asMap().entries.map((entry) {
+                return GestureDetector(
+                  onTap: () => _carouselController.animateToPage(entry.key),
+                  child: Container(
+                    width: 12.0,
+                    height: 12.0,
+                    margin: const EdgeInsets.symmetric(
+                        vertical: 8.0, horizontal: 4.0),
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: (Theme.of(context).brightness == Brightness.dark
+                                ? Colors.white
+                                : Colors.black)
+                            .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+                  ),
+                );
+              }).toList(),
+            ),
         ],
       ),
     );
@@ -636,13 +805,6 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
       await customErrorDialog(
           context, 'Error', 'Obra Terminada. No se puede eliminar fotos.');
 
-      // await showAlertDialog(
-      //     context: context,
-      //     title: 'Error',
-      //     message: 'Obra Terminada. No se puede eliminar fotos.',
-      //     actions: <AlertDialogAction>[
-      //       const AlertDialogAction(key: null, label: 'Aceptar'),
-      //     ]);
       return;
     }
 
@@ -650,13 +812,6 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
       await customErrorDialog(context, 'Error',
           'Su usuario no está habilitado para eliminar Fotos.');
 
-      // await showAlertDialog(
-      //     context: context,
-      //     title: 'Error',
-      //     message: 'Su usuario no está habilitado para eliminar Fotos.',
-      //     actions: <AlertDialogAction>[
-      //       const AlertDialogAction(key: null, label: 'Aceptar'),
-      //     ]);
       return;
     }
 
@@ -664,28 +819,11 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
       await customErrorDialog(context, 'Error',
           'Esta foto (NROREGISTRO ${_obra.obrasDocumentos[_current].nroregistro}) sólo puede ser eliminada por el Usuario que la cargó (${_obra.obrasDocumentos[_current].generadoPor}). De ser necesario borrarla comuníquese con el administrador del Sistema.');
 
-      // await showAlertDialog(
-      //     context: context,
-      //     title: 'Error',
-      //     message:
-      //         'Esta foto (NROREGISTRO ${_obra.obrasDocumentos[_current].nroregistro}) sólo puede ser eliminada por el Usuario que la cargó (${_obra.obrasDocumentos[_current].generadoPor}). De ser necesario borrarla comuníquese con el administrador del Sistema.',
-      //     actions: <AlertDialogAction>[
-      //       const AlertDialogAction(key: null, label: 'Aceptar'),
-      //     ]);
       return;
     }
 
     var response = await customYesNoQuestionDialog(context,
         content: '¿Estas seguro de querer borrar esta foto?');
-
-    // await showAlertDialog(
-    //     context: context,
-    //     title: 'Confirmación',
-    //     message: '¿Estas seguro de querer borrar esta foto?',
-    //     actions: <AlertDialogAction>[
-    //       const AlertDialogAction(key: 'no', label: 'No'),
-    //       const AlertDialogAction(key: 'yes', label: 'Sí'),
-    //     ]);
 
     if (response == 'yes') {
       await _deletePhoto();
@@ -703,13 +841,6 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
       await customErrorDialog(
           context, 'Error', 'Verifica que estés conectado a Internet');
 
-      // await showAlertDialog(
-      //     context: context,
-      //     title: 'Error',
-      //     message: 'Verifica que estes conectado a internet.',
-      //     actions: <AlertDialogAction>[
-      //       const AlertDialogAction(key: null, label: 'Aceptar'),
-      //     ]);
       return;
     }
 
@@ -721,13 +852,6 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
     if (!response.isSuccess) {
       await customErrorDialog(context, 'Error', 'response.message');
 
-      // await showAlertDialog(
-      //     context: context,
-      //     title: 'Error',
-      //     message: response.message,
-      //     actions: <AlertDialogAction>[
-      //       const AlertDialogAction(key: null, label: 'Aceptar'),
-      //     ]);
       return;
     }
 
@@ -795,9 +919,7 @@ class _ObraInfoScreenState extends State<ObraInfoScreen> {
     SnackBar snackbar = SnackBar(
       content: Text(text),
       backgroundColor: Colors.lightGreen,
-      //duration: Duration(seconds: 3),
     );
     ScaffoldMessenger.of(context).showSnackBar(snackbar);
-    //ScaffoldMessenger.of(context).hideCurrentSnackBar();
   }
 }
